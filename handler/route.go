c772d391f8538e7322baf140370ff1b9bfab8d85
= <=>
2024-07-10 19:15:09 +0200

    template update

--- handler/route.go
+++ handler/route.go
@@ -1,11 +1,15 @@
 package handler
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"gohtmx/lib"
 	"html/template"
 	"io"
 	"log"
+	"mime"
+	"mime/multipart"
 	"net/http"
 )
 
@@ -16,8 +20,16 @@ type RouteInterface interface {
 	Patch()
 }
 
+type Multipart struct{
+    Body map[string]string
+    File io.Reader
+}
+
+type ErrToken error
+
 type Route struct {
 	RouteInterface
+    Multipart Multipart
 	Params     map[string]string
 	Request    *http.Request
 	Response   http.ResponseWriter
@@ -25,19 +37,52 @@ type Route struct {
     ContentType string
 }
 
-func NewRoute(response http.ResponseWriter, request *http.Request) *Route {
+func NewRoute(response http.ResponseWriter, request *http.Request) (*Route, ErrToken) {
+
+    var multip Multipart = Multipart{Body: map[string]string{}}
 	contentType := request.Header.Get("Content-Type")
+    token, err := request.Cookie("x-auth")
+    err = VerifyToken(token.String())
+    ct, params, _ := mime.ParseMediaType(request.Header.Get("Content-Type"))
 	var urlEncoded map[string]string = map[string]string{}
 	if contentType == "application/x-www-form-urlencoded" {
 		urlEncoded = lib.ReadBody(request.Body)
-	}	
+    }else if ct == "multipart/form-data"{
+        reader := multipart.NewReader(request.Body, params["boundary"])
+        body := make([]byte, 1024)
+        var payload string
+        Parts:
+        for{
+            part, err := reader.NextPart()
+            if err == io.EOF{
+                break Parts
+            }
+            defer part.Close()
+            var fileBuffer bytes.Buffer
+            Exit:
+            for {
+                bytesReaded, err := part.Read(body)
+                fileBuffer.Write(body[:bytesReaded])
+                payload = string(body[:bytesReaded])
+                if err == io.EOF {break Exit}
+            }
+            
+            if part.FileName() != ""{
+                multip.File = &fileBuffer
+                continue
+            }
+            multip.Body[part.FormName()] = payload
+            
+        }
+    }	
     return &Route{
         ContentType: contentType,
+        Multipart: multip,
 		Request:    request,
 		Response:   response,
 		Params:     map[string]string{},
 		UrlEncoded: urlEncoded,
-	}
+	}, err
 }
 
 func DecodeJson(jsonStruct any, body io.ReadCloser){
@@ -96,3 +141,9 @@ func (r *Route) Render(writer http.ResponseWriter, payload interface{}, files ..
     }
 }
 
+func VerifyToken(token string)error{
+    if token == ""{
+        return errors.New("unauthorized")
+    }
+    return nil
+}
